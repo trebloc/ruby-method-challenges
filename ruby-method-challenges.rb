f9964ac4264bafe21c875831d5386ac90196fa57
trebloc <ctse77@alum.berkeley.edu>
2016-01-03 23:21:53 -0800

    worked on 2nd exercise

--- ruby-method-challenges.rb
+++ ruby-method-challenges.rb
@@ -6,3 +6,33 @@
 
 # combineWords( 'dog', 'house' );
 # // displays 'doghouse'
+
+# output a String and a parameter to the display
+def combineWords(word1, word2)
+  word1 + word2
+end
+
+word1 = 'dog'
+word2 = 'house'
+puts combineWords(word1, word2)
+
+# 2. Repeat a phrase
+
+# Display an argument phrase to the console n times
+
+# function repeatPhrase( phrase, n ) {
+#     // TODO: Place your code here
+# }
+
+# repeatPhrase ( "Hello", 5);
+# // displays 
+# // Hello
+# // Hello
+# // Hello
+# // Hello
+# // Hello
+
+5.times { puts "Hello" }
+
+
+
